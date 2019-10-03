6e4391f72f5b4ee0661645bc145d1112b9c5c78e
Yuji Teshima <yuji.teshima@gmail.com>
2019-10-03 12:56:15 +0900

    nuxtconfig titleを可変設定にする

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: '%s | Teshi Blog',
     meta: [{
         charset: 'utf-8'
       },
